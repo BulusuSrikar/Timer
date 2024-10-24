e98f1b3c34bbc68011eef34a9f45ecfaf7433830
Bulusu Srikar <148469434+BulusuSrikar@users.noreply.github.com>
2024-10-24 11:26:19 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import Timer from './Timer'; // Import the Timer component
+
+function App() {
+  return (
+    <div className="App">
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
